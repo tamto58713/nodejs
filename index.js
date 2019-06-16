fa8c99378f913b859b72a2b9bcef1751b0b42014
Tam To Tran <tamtotran5667@gmail.com>
2019-06-17 04:37:48 +0700

    learn lowdb && shortid

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
 const express = require('express')
+const bodyParser = require("body-parser")
+const port = process.env.PORT || 8080
+const low = require('lowdb')
+const shortid = require('shortid')
 app = express();
 app.set('view engine', 'pug')
 app.set('views', './views')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+db.defaults({users: []}).write();
 app.get('/', (req, res) => {
-    res.render('header', {name: "Tam To Tran"})
+    res.render('')
 })
-var users= [{name: "Tam To", age: 19}, {name: "Abel", age: 20}]
+
 
 app.get('/users', (req, res) => {
-    res.render('users', {users});
+    res.render('users/index', {users: db.get('users').value()});
 })
 app.get('/users/search', (req, res) => {
     let q = req.query.q;
-    console.log(q)
+    const users = db.get('users').value();
     let usersMatches = users.filter((user) => {
-        console.log(q + ' ' + user.name)
-        console.log(user.name.indexOf(q))
         return user.name.toLowerCase().indexOf(q.toLowerCase()) !==-1;
     })
-    res.render("users", {users: usersMatches})
+    res.render("users/index", {users: usersMatches})
 
 });
-app.listen(8080)
+app.get('/users/create', (req, res) => {
+    res.render('users/create')
+})
+app.post('/users/create', (req, res) => {
+    req.body.id = shortid.generate();
+    db.get('users').push(req.body).write();
+    res.redirect('/users')
+})
+app.get('/users/:id', (req, res) => {
+    let id = req.params.id;
+    let user = db.get('users').find({id: id}).value()
+    res.render('users/view', {user: user})
+})
+app.listen(8080, () => {
+    console.log('Server listening on port', port)
+})
+
