3cefc85d746d88a33e889e14578f114f51114012
Tam To Tran <tamtotran5667@gmail.com>
2019-06-17 02:26:57 +0700

    learn search use req.query

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ app.set('views', './views')
 app.get('/', (req, res) => {
     res.render('header', {name: "Tam To Tran"})
 })
-app.get('/users', (req, res) => {
-    res.render('users', {users: [{name: "Tam To", age: 19}, {name: "Abel", age: 20}]})
-})
+var users= [{name: "Tam To", age: 19}, {name: "Abel", age: 20}]
 
+app.get('/users', (req, res) => {
+    res.render('users', {users});
+})
+app.get('/users/search', (req, res) => {
+    let q = req.query.q;
+    console.log(q)
+    let usersMatches = users.filter((user) => {
+        console.log(q + ' ' + user.name)
+        console.log(user.name.indexOf(q))
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !==-1;
+    })
+    res.render("users", {users: usersMatches})
+
+});
 app.listen(8080)
