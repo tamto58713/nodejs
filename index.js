064bee0db2793485702b095594b69a51a3c812ef
Tam To Tran <tamtotran5667@gmail.com>
2019-06-17 01:59:17 +0700

    learn template engine each

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 app = express();
 app.set('view engine', 'pug')
+app.set('views', './views')
 app.get('/', (req, res) => {
-    res.render('header')
+    res.render('header', {name: "Tam To Tran"})
 })
 app.get('/users', (req, res) => {
-    res.render('users')
+    res.render('users', {users: [{name: "Tam To", age: 19}, {name: "Abel", age: 20}]})
 })
 
 app.listen(8080)
