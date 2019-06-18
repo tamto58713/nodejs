eee20fef82ebc92cf7a32ead8382283c24c40672
Tam To Tran <tamtotran5667@gmail.com>
2019-06-18 22:58:22 +0700

    static file for study

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.set('view engine', 'pug')
 app.set('views', './views')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.render('')
